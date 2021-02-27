3d4f221cfb3126b67a20f2e209be6ed8e80f342f
Serhiy <kosovchychserhiy@gmail.com>
2021-02-27 12:25:44 +0200

    Make design for IssueItem

--- it-frontend/src/screens/dashboard/issues/components/IssueItem.jsx
+++ it-frontend/src/screens/dashboard/issues/components/IssueItem.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Row } from 'antd';
+import { Row, Col, Space } from 'antd';
 import { Link } from 'react-router-dom';
+import { ClockCircleOutlined } from '@ant-design/icons';
 
 function IssueItem(issue) {
   return (
     <div className="issue-item">
-      <Row>
-        <Link to="#" className="issue-item-exception">RedisException</Link>
+      <Row className="issue-item-header">
+        <Link to="#" className="issue-item-exception gray">
+          <span className="issue-exception-title">
+            RedisException
+          </span>
+          <span className="issue-class-path">
+            /app/Models/Robots/Documents/validate/Jobs/ValidateDocument.php in App\Models\Robots\Documents\validate\Jobs\ValidateDocument::performTask
+          </span>
+        </Link>     
       </Row>
-      <Row>
-        Path cannot be empty
+      <Row className="issue-exception-message">      
+        Path cannot be empty Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque magni illum repudiandae? Accusantium tenetur labore ipsum illo tempora ullam culpa vitae quas autem quod error porro dolorem est, dolore voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum pariatur saepe laudantium quas voluptate totam error placeat ipsum assumenda cupiditate. Excepturi reiciendis ad magnam debitis quibusdam. Commodi aliquid veniam nobis!
       </Row>
-      <Row>
-        ROBOT-2TA
-      </Row>    
+      <Row className="issue-parameters gray d-flex" align="middle">
+        <Col>
+          <Space align="center" size='middle'>
+            <span>ROBOT-2TA</span>
+            <span><ClockCircleOutlined /> 29.01.2020 16:53 - 30.03.2020 17:56</span>
+            <span>php</span>
+          </Space>
+        </Col>
+      </Row>
     </div>    
   );
 }
--- it-frontend/src/screens/dashboard/issues/components/IssuesTable.jsx
+++ it-frontend/src/screens/dashboard/issues/components/IssuesTable.jsx
@@ -36,11 +36,12 @@ function IssuesTable() {
 
   return (
     <Table
+      tableLayout='fixed'
       className="issues-table"
       rowSelection={{ type: 'checkbox', ...rowSelection }}
       dataSource={data}
     >
-      <Column title="Issue" dataIndex="issue" key="issue" render={(_, record) => IssueItem(record.issue)} />
+      <Column title="Issue" dataIndex="issue" key="issue" width="95%" render={(_, record) => IssueItem(record.issue)} />
       <Column title="Events" dataIndex="events" key="events" width="5%" align="center" />
     </Table>
   );
