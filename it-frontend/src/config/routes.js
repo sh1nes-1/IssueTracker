49e2ab1ecdd10d2d6596de84211c4670bdccd95d
Serhiy <kosovchychserhiy@gmail.com>
2021-03-12 11:42:12 +0200

    Make default path redirecting to dashboard

--- it-frontend/src/config/routes.js
+++ it-frontend/src/config/routes.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
 import App from "../screens/App";
 import auth_config from "../screens/auth/routes";
 import dashboard_config from "../screens/dashboard/routes";
@@ -7,6 +9,11 @@ const routes = [
   {
     component: App,
     routes: [
+      {
+        path: '/',
+        exact: true,
+        component: () => <Redirect to="/dashboard/projects"/>
+      },
       ...auth_config,
       ...dashboard_config,
       
